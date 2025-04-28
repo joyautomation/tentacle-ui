dcd98643c84a80ae629053b095b8b3abae707152
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2025-04-28 11:06:27 -0700

    layout updates (#1)
    * iwp
    
    * wip
    
    * wip
    
    * added dockerfile and github actions to build/register to both public and private container registries
    
    * 0.0.2
    
    * fixed tag detection
    
    * 0.0.3
    
    * disabled csrf
    
    * 0.0.4
    
    ---------
    
    Co-authored-by: James A Joy <joyja@mira1.internal1.jarautomation.io>
    Co-authored-by: James Joy <joyja@joyautomation.com>

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false,
+		},
+	},
 	extensions: [".svelte", ".svx"],
 };
 
